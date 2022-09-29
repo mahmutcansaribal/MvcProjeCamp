5ae7b53a9f755a0abf88a7659fc5411c36d695f1
Mahmut Can SARIBAL <mahmutcansaribal_19@hotmail.com>
2022-09-29 17:14:07 +0300

    Controller and AddCategory added.

---
+++ MvcProjeKampi/Controllers/CategoryController.cs
@@ -0,0 +1,55 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class CategoryController : Controller
+    {
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetCategoryList()
+        {
+            //todo category verilerini categoryManager dan çektik.
+            var categoryvalues = cm.GetList();
+            return View(categoryvalues);
+        }
+        //todo Kategori ekleme işlemi
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddCategory(Category p)
+        {
+            CategoryValidator categoryvalidator = new CategoryValidator();
+            //todo validate kurallarımız için parametre yolluyoruz
+            ValidationResult result = categoryvalidator.Validate(p);
+            if (result.IsValid)
+            {
+                cm.CategoryAdd(p);
+                return RedirectToAction("GetCategoryList");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+    }
+}
