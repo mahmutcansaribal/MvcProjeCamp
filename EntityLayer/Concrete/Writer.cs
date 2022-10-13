9f67c71a0a1d01f350a1556e1b59180b4ca4e8bf
Mahmut Can SARIBAL <mahmutcansaribal_19@hotmail.com>
2022-10-13 18:11:05 +0300

    EntityLayer Uptaded.

--- EntityLayer/Concrete/Writer.cs
+++ EntityLayer/Concrete/Writer.cs
@@ -16,10 +16,12 @@ namespace EntityLayer.Concrete
         [StringLength(50)]
         public string WriterSurname { get; set; }
         [StringLength(100)]
+        public string WriterAbout { get; set; }
+        [StringLength(100)]
         public string WriterImage { get; set; }
-        [StringLength(50)]
+        [StringLength(200)]
         public string WriterMail { get; set; }
-        [StringLength(20)]
+        [StringLength(200)]
         public string WriterPassword { get; set; }
         
         //İLİŞKİLER
