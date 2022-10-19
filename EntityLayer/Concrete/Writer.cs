4352f53dffb64acea8216696b94d8e58683c44e8
Mahmut Can SARIBAL <mahmutcansaribal_19@hotmail.com>
2022-10-19 17:55:19 +0300

    add field to writer.

--- EntityLayer/Concrete/Writer.cs
+++ EntityLayer/Concrete/Writer.cs
@@ -17,7 +17,7 @@ namespace EntityLayer.Concrete
         public string WriterSurname { get; set; }
         [StringLength(100)]
         public string WriterAbout { get; set; }
-        [StringLength(100)]
+        [StringLength(250)]
         public string WriterImage { get; set; }
         [StringLength(200)]
         public string WriterMail { get; set; }
