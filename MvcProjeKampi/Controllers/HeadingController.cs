dce8a868b47b6db2fe0f89c97529c2b338d17ce1
Mahmut Can SARIBAL <mahmutcansaribal_19@hotmail.com>
2022-11-22 19:58:19 +0300

    UI

--- MvcProjeKampi/Controllers/HeadingController.cs
+++ MvcProjeKampi/Controllers/HeadingController.cs
@@ -50,5 +50,40 @@ namespace MvcProjeKampi.Controllers
             hm.HeadingAdd(p);
             return RedirectToAction("Index");
         }
+        [HttpGet]
+        public ActionResult EditHeading(int id)
+        {
+            List<SelectListItem> valuecategory = (from x in cm.GetList()
+                                                  select new SelectListItem
+                                                  {
+                                                      Text = x.CategoryName,
+                                                      Value = x.CategoryID.ToString()
+                                                  }).ToList();
+            ViewBag.vlc = valuecategory;
+            var headingValue = hm.GetByID(id);
+            return View(headingValue);
+        }
+        [HttpPost]
+        public ActionResult EditHeading(Heading p)
+        {
+            hm.HeadingUpdate(p);
+            return RedirectToAction("Index");
+        }
+        public ActionResult DeleteHeading(int id)
+        {
+            //id ye göre bulduk
+            var HeadingValue = hm.GetByID(id);
+            if(HeadingValue.HeadingStatus == false)
+            {
+                HeadingValue.HeadingStatus = true;
+            }
+            else
+            {
+                HeadingValue.HeadingStatus = false;
+            }
+            hm.HeadingDelete(HeadingValue);
+            return RedirectToAction("Index");
+        }
+       
     }
 }
